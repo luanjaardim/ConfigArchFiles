368c9b861764855f23b3403528f04279305510a7
luanjaardim <luang.ajardim@gmail.com>
2024-06-14 20:44:14 -0300

    Same with neovim configuration, only adding transparency.

--- hyprland/nvim/init.lua
+++ hyprland/nvim/init.lua
@@ -805,9 +805,13 @@ require("lazy").setup({
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
 		"folke/tokyonight.nvim",
+
 		priority = 1000, -- make sure to load this before all the other start plugins
 		init = function()
 			-- Load the colorscheme here.
+			require("tokyonight").setup({
+				transparent = true,
+			})
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
 			vim.cmd.colorscheme("tokyonight-night")
